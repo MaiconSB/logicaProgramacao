139440f8308e9c1ac07b5011d63f21c3fa3e66d7
Maicon Silveira <maiconsb2041@gmail.com>
2023-01-15 12:49:24 -0300

    Exercicio de repatição parte 2 arrays

--- Exercicios/Consoantes/src/Consoantes.java
+++ Exercicios/Consoantes/src/Consoantes.java
@@ -1,5 +1,27 @@
+/* Faça um programa que leia um vetor de seis caracteres, e 
+ * diga quantas consoantes foram lidas. Imprima as consoantes. 
+ */
+import java.util.Scanner; 
+
 public class Consoantes {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in); 
+
+        String[] letras = new String[6]; 
+        int quantidadeConsoantes = 0; 
+
+        for (int i = 0; i < letras.length; i++) {
+            System.out.print("Letra: ");
+            String letra = scan.next(); 
+            if (letra.equals("a") || letra.equals("e") || letra.equals("i") || letra.equals("o") || 
+            letra.equals("u")) {
+
+            } else {
+                quantidadeConsoantes = quantidadeConsoantes + 1;
+            }
+        }
+
+        System.out.println("Quantidade de consoantes: " + quantidadeConsoantes);
+
     }
 }
