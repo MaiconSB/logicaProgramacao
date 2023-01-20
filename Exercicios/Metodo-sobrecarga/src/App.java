03901c5d54c19afe30721c66534a3e9c531f650a
Maicon Silveira <maiconsb2041@gmail.com>
2023-01-20 18:53:09 -0300

    Exercicio sobre metodos sobrecargas

---
+++ Exercicios/Metodo-sobrecarga/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Exercicio quadrilátero: ");
+            Quadrilatero.area(3); 
+            Quadrilatero.area(5,5); 
+            Quadrilatero.area(7,8,9);
+            Quadrilatero.area(5, 5);
+
+        
+    }
+}
