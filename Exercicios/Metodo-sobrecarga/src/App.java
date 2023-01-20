5bf478600249ed8d4349a22fba5aea8fe8d3f581
Maicon Silveira <maiconsb2041@gmail.com>
2023-01-20 19:25:32 -0300

    Usando o return nos métodos

--- Exercicios/Metodo-sobrecarga/src/App.java
+++ Exercicios/Metodo-sobrecarga/src/App.java
@@ -1,11 +1,18 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Exercicio quadrilátero: ");
-            Quadrilatero.area(3); 
-            Quadrilatero.area(5,5); 
-            Quadrilatero.area(7,8,9);
-            Quadrilatero.area(5, 5);
+           double areaQuadrado = Quadrilatero.area(3);
+           System.out.println("Área do quadrado: " + areaQuadrado);
 
+           double areaRetangulo = Quadrilatero.area(5,5);
+           System.out.println("Área do quadrado: " + areaRetangulo);
+
+           double areaTrapezio = Quadrilatero.area(7,8,9);
+           System.out.println("Área do quadrado: " + areaTrapezio);
+
+           double areaDiagonal = Quadrilatero.area(5f,5f);
+           System.out.println("Área do quadrado: " + areaDiagonal);
+            
         
     }
 }
